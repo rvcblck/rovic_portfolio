66171eefb267713f4f2b4e83067f88e1ce61cbff
Rovic G. De Leon <mariomanayon77@gmail.com>
2024-03-29 21:17:15 +0800

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/rovic_portfolio",
+  assetPrefix: "/rovic_portfolio",
   images: { unoptimized: true },
   experimental: { images: { unoptimized: true } },
   swcMinify: true,

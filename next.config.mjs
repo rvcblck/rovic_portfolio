32dea11078862ad4d94db49b789befd3d97c1ae8
Rovic G. De Leon <mariomanayon77@gmail.com>
2024-03-29 20:20:46 +0800

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/rovic_portfolio",
-  output: "export", // <=== enables static exports
+  output: "export",
   reactStrictMode: true,
 };
 
